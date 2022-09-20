f6168ed45aacf62d7d2535ade148ca9865f391cd
Elena Sveshnikova <lenamarinkevich@mail.ru>
2022-09-20 20:26:02 +0300

    fix cloneNode

--- scripts/index.js
+++ scripts/index.js
@@ -4,15 +4,13 @@ const buttonSend = document.querySelector('.main__btn-send')
 
 // todo
 const todoListContainer = document.querySelector('.main__todo-list')
-const todoTemplate = document.querySelector('.main__list')
-
 
 
 // add todo function
 function createTodo (text) {
-    const todoTemplate = document.querySelector('.main__list');
-    const todoElement = todoTemplate.querySelector('.todo').cloneNode(true);
-    
+    const todoTemplate = document.querySelector('.main__list').content;;
+    const todoElement = todoTemplate.querySelector('.todo').cloneNode(true);;
+    console.log(todoElement);
     todoElement.querySelector('.todo__text').textContent = text;
 
     // edit
@@ -29,7 +27,8 @@ function addElementInContainer(element,container) {
   } 
 
   // добавление массива дел
-  list.forEach(item => {
+  list.reverse().forEach(item => {
     const todo = createTodo(item);
     addElementInContainer(todo , todoListContainer)
   });
+
